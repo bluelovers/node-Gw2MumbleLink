4db02480cd96b705f790119c3fa297383c305ed3
bluelovers <codelovers@users.sourceforge.net>
2016-03-28 15:49:29 +0800

    . (Step: 1)
    Step: 1
    
    [ M] modified - Changes not staged for commit:
    
    	Gw2MumbleLink/Class1.cs
    	Gw2MumbleLink/Gw2MumbleLink.csproj
    	Gw2MumbleLink/Properties/AssemblyInfo.cs
    	Gw2MumbleLink/packages.config

--- Gw2MumbleLink/Class1.cs
+++ Gw2MumbleLink/Class1.cs
@@ -1,7 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Gw2MumbleLink
 {
