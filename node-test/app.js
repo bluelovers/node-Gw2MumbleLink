285b6a17a664cc92a82f1824b63b52b08d091d76
bluelovers <codelovers@users.sourceforge.net>
2016-03-28 19:14:01 +0800

    update copy (Step: 1)
    Step: 1
    
    [ M] modified - Changes not staged for commit:
    
    	app.js

--- node-test/app.js
+++ node-test/app.js
@@ -2,7 +2,7 @@
 
 var edge = require('edge');
 
-var objDLL = edge.func({
+var objDLL2 = edge.func({
 	
 	assemblyFile: 'Gw2MumbleLink.dll',
 	
@@ -12,7 +12,37 @@ var objDLL = edge.func({
 
 });
 
+objDLL2('objDLL2', function (error, result) {
+	if (error) throw error;
+	console.log(result);
+});
+
+var objDLL = edge.func({
+	
+	assemblyFile: 'Gw2MumbleLink.dll',
+	
+	typeName: 'Gw2MumbleLink.MainClass',
+	
+	methodName: 'Init'
+
+});
+
 objDLL('objDLL', function (error, result) {
+	if (error) throw error;
+	console.log(result);
+});
+
+var objDLL2 = edge.func({
+	
+	assemblyFile: 'Gw2MumbleLink.dll',
+	
+	typeName: 'Gw2MumbleLink.MainClass',
+	
+	methodName: 'test'
+
+});
+
+objDLL2('objDLL2', function (error, result) {
 	if (error) throw error;
 	console.log(result);
 });
